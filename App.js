0128feec5000cdefaf8b1883345fe316d65f5041
Prabesh Paudel <45585088+prabeshpaudel@users.noreply.github.com>
2024-04-17 23:14:29 -0400

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,8 @@ import "dotenv/config";
 
 const app = express();
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb+srv://rudaiba:kanbas123@cluster0.eyxehye.mongodb.net/Kanbas?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(CONNECTION_STRING);
 //const db = mongoose.connection;
 //db.on('error', console.error.bind(console, 'connection error:'));
